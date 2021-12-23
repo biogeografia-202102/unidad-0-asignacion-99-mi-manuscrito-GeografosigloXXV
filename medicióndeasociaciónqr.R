4ae9bf01214716a2fd704d8d833d56e4ce1ccf9e
GeografosigloXX <duroeljoselito@hotmail.com>
2021-12-23 12:34:24 -0400

    reproducción de scripts

---
+++ medicióndeasociaciónqr.R
@@ -0,0 +1,53 @@
+library(vegan)
+## Loading required package: permute
+
+## Loading required package: lattice
+
+## This is vegan 2.5-6
+library(adespatial)
+## Registered S3 methods overwritten by 'adegraphics':
+##   method         from
+##   biplot.dudi    ade4
+##   kplot.foucart  ade4
+##   kplot.mcoa     ade4
+##   kplot.mfa      ade4
+##   kplot.pta      ade4
+##   kplot.sepan    ade4
+##   kplot.statis   ade4
+##   scatter.coa    ade4
+##   scatter.dudi   ade4
+##   scatter.nipals ade4
+##   scatter.pco    ade4
+##   score.acm      ade4
+##   score.mix      ade4
+##   score.pca      ade4
+##   screeplot.dudi ade4
+
+## Registered S3 method overwritten by 'spdep':
+##   method   from
+##   plot.mst ape
+
+## Registered S3 methods overwritten by 'adespatial':
+##   method             from       
+##   plot.multispati    adegraphics
+##   print.multispati   ade4       
+##   summary.multispati ade4
+library(tidyverse)
+## ── Attaching packages ──────────────────────────────────── tidyverse 1.2.1 ──
+
+## ✓ ggplot2 3.3.2     ✓ purrr   0.3.4
+## ✓ tibble  3.0.3     ✓ dplyr   0.8.3
+## ✓ tidyr   1.0.0     ✓ stringr 1.4.0
+## ✓ readr   1.3.1     ✓ forcats 0.4.0
+
+## ── Conflicts ─────────────────────────────────────── tidyverse_conflicts() ──
+## x dplyr::filter() masks stats::filter()
+## x dplyr::lag()    masks stats::lag()
+library(gridExtra)
+## 
+## Attaching package: 'gridExtra'
+
+## The following object is masked from 'package:dplyr':
+## 
+##     combine
+source('biodata/funciones.R')
