f5b5347430ed77ea86cdd8580b63daed56d2ca4c
GeografosigloXX <duroeljoselito@hotmail.com>
2022-01-12 11:40:42 -0400

    Añadido scripts reproducibles

--- analisisagrupamientojerarquico.R
+++ analisisagrupamientojerarquico.R
@@ -17,8 +17,8 @@ source('biodata/funciones.R')
 
 #' ### Cargar datos
 #' 
-load('biodata/Apocynaceae-Meliaceae-Sapotaceae.Rdata')
-mi_fam <- mc_apcyn_melic_saptc
+load('biodata/Moraceae.Rdata')
+mi_fam <- mc_morac
 #'
 #' ## Características de las técnicas de agrupamiento
 #' 
@@ -63,7 +63,7 @@ attr(mi_fam_norm_d, "labels") <- rownames(mi_fam)
 #' 
 #' Finalmente, el dendrograma a continuación:
 plot(cl_single, labels = rownames(mi_fam), hang = -1,
-     main = "Sitios de BCI según composición de especies de Apocynaceae, Meliaceae, Sapotaceae\nEnlace simple a partir de matriz de distancia de cuerdas",
+     main = "Sitios de BCI según composición de especies de Moraceae\nEnlace simple a partir de matriz de distancia de cuerdas",
      xlab = 'Sitios', ylab = 'Altura')
 #' 
 #' ### Agrupamiento "aglomerativo" por enlace completo
@@ -72,7 +72,7 @@ plot(cl_single, labels = rownames(mi_fam), hang = -1,
 #' 
 (cl_complete <- hclust(mi_fam_norm_d, method = 'complete'))
 plot(cl_complete, labels = rownames(mi_fam), hang = -1,
-     main = "Sitios de BCI según composición de especies de Apocynaceae, Meliaceae, Sapotaceae\nEnlace completo a partir de matriz de distancia de cuerdas",
+     main = "Sitios de BCI según composición de especies de Moraceae\nEnlace completo a partir de matriz de distancia de cuerdas",
      xlab = 'Sitios', ylab = 'Altura')
 #' 
 #' ### Agrupamiento "aglomerativo" por enlace promedio
@@ -85,7 +85,7 @@ plot(cl_complete, labels = rownames(mi_fam), hang = -1,
 #' 
 (cl_upgma <- hclust(mi_fam_norm_d, method = 'average'))
 plot(cl_upgma, labels = rownames(mi_fam), hang = -1,
-     main = "Sitios de BCI según composición de especies de Apocynaceae, Meliaceae, Sapotaceae\nUPGMA a partir de matriz de distancia de cuerdas",
+     main = "Sitios de BCI según composición de especies de Moraceae\nUPGMA a partir de matriz de distancia de cuerdas",
      xlab = 'Sitios', ylab = 'Altura')
 #' 
 #' ### Agrupamiento por el método de Ward de varianza mínima
@@ -94,5 +94,5 @@ plot(cl_upgma, labels = rownames(mi_fam), hang = -1,
 #' 
 (cl_ward <- hclust(mi_fam_norm_d, method = 'ward.D2'))
 plot(cl_ward, labels = rownames(mi_fam), hang = -1,
-     main = "Sitios de BCI según composición de especies de Apocynaceae, Meliaceae, Sapotaceae\nMétodo de Ward a partir de matriz de distancia de cuerdas",
+     main = "Sitios de BCI según composición de especies de Moraceae\nMétodo de Ward a partir de matriz de distancia de cuerdas",
      xlab = 'Sitios', ylab = 'Altura')
